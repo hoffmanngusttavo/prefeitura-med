921501889f85216f0a6a1132bab73d9616989f47
Gustavo H. <hoffmann.gusttavo@gmail.com>
2024-06-07 20:36:44 -0300

    #task_batch[ - inserido controller upload arquivo cnab com spring batch]

--- src/main/java/com/api/prefeitura/medicamento/model/entity/TransacaoCNAB.java
+++ src/main/java/com/api/prefeitura/medicamento/model/entity/TransacaoCNAB.java
@@ -1,6 +1,5 @@
 package com.api.prefeitura.medicamento.model.entity;
 
-import com.api.prefeitura.medicamento.model.dto.TransacaoCNABDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -10,7 +9,6 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 @Entity
 @Table(name = "transacao")
@@ -42,32 +40,4 @@ public class TransacaoCNAB {
     private String nomeDaLoja;
 
 
-    public TransacaoCNAB construct(TransacaoCNABDTO item) {
-
-        this.id = null;
-        this.tipo = item.tipo();
-        this.cpf = item.cpf();
-        this.cartao = item.cartao();
-        this.donoDaLoja = item.donoDaLoja();
-        this.nomeDaLoja = item.nomeDaLoja();
-
-        return this;
-    }
-
-    public TransacaoCNAB withValor(BigDecimal valorFormatado) {
-        this.valor = valorFormatado;
-        return this;
-    }
-
-    public TransacaoCNAB withData(String data) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-        this.data = LocalDate.parse(data, formatter);
-        return this;
-    }
-
-    public TransacaoCNAB withHour(String hora) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HHmmss");
-        this.data = LocalDate.parse(hora, formatter);
-        return this;
-    }
 }
--- src/main/java/com/api/prefeitura/medicamento/model/service/CnabService.java
+++ src/main/java/com/api/prefeitura/medicamento/model/service/CnabService.java
@@ -2,7 +2,6 @@ package com.api.prefeitura.medicamento.model.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 
 public interface CnabService {
     void updloadCnabFile(MultipartFile file) throws Exception;
